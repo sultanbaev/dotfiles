b3d80a5116217b9cede9db0fbe2961d596bc7807
sultanbaev <sultanbaev.vitaly@gmail.com>
2016-03-23 22:44:30 +0600

    arch i3

---
+++ scripts/pacman.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+sudo pacman -S dropbox lm_sensors mplayer transmission-cli transmission-gtk guake mc homebank wine feh linux-headers linux-lts-headers scrot conky gimp gparted spacefm lxappearence --noconfirm
--- scripts/with_pacman.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-sudo pacman -S dropbox lm_sensors mplayer transmission-cli transmission-gtk guake mc homebank wine feh linux-lts-headers scrot --noconfirm
